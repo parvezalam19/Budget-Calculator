36aba1b3f87a730ccfb4306ba766d6baae8c94ab
Abdullah Saif <parvezalam9970@gmail.com>
2022-09-14 12:09:58 +0530

    Updates

---
+++ precache-manifest.c1f4eaa42943c4b8723e74a777c66a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "046c713f2597584f3bf8",
+    "url": "/Budget-Calculator/static/css/main.0afce02b.chunk.css"
+  },
+  {
+    "revision": "046c713f2597584f3bf8",
+    "url": "/Budget-Calculator/static/js/main.046c713f.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/Budget-Calculator/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "7010d5d4df71bdbc774e",
+    "url": "/Budget-Calculator/static/js/2.7010d5d4.chunk.js"
+  },
+  {
+    "revision": "d2fdabaebc9aa427252c",
+    "url": "/Budget-Calculator/static/js/runtime~main.d2fdabae.js"
+  },
+  {
+    "revision": "7eeef07bf0f4bdcb6a87f73c82d0e00f",
+    "url": "/Budget-Calculator/index.html"
+  }
+];
